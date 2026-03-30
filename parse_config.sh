f4bb7eed665f7534f6463ff257a3df39b9a0bfd2
daemonnd <daemonnd@protonmail.com>
2026-03-30 11:01:50 +0200

    add parse_config.sh

---
+++ parse_config.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# strict mode
+set -Eeuo pipefail
+
+# Cleanup function
+function cleanup {
+    local exit_code="$?"
+    echo "Script parse_config.sh interrupted or failed. Cleaning up..."
+
+    # remove tmp files
+
+    # exit the script, preserving the exit code
+    exit "$exit_code"
+}
+
+# trap errors
+trap 'echo "Error on line $LINENO in parse_config.sh: command \"$BASH_COMMAND\" exited with status $?" >&2' ERR
+# trap signals
+trap 'cleanup' INT TERM ERR
+
+function check_args {
+    echo
+}
+
+function main {
+    cat /dev/stdin | awk ' {
+    gsub(/\/\/.*/, "") 
+    print $0
+    }
+    '
+}
+# call main with all args, as given
+main "$@"
