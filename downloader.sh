a5e85e4214e59179676740dd26982c056c832ad9
daemonnd <daemonnd@protonmail.com>
2026-04-01 16:33:52 +0200

    add init function to downloader.sh to use the yt-dlp general and yt-dlp specific args set in the config.jsonc file

--- downloader.sh
+++ downloader.sh
@@ -25,6 +25,17 @@ function check_args {
     echo
 }
 
+function init {
+    declare -a yt_dlp_args
+    declare -a download_specific_yt_dlp_args
+    # get the additional yt-dlp args used for every yt-dlp call from the parsed config file
+    mapfile -t yt_dlp_args < <(cat "$VIDSIFT_DATA_DIR"/parsed_config.json | jq -r '.general_processing."yt-dlp_args"[]')
+    cat "$VIDSIFT_DATA_DIR"/parsed_config.json | jq -r '.general_processing."yt-dlp_args"[]'
+    # get the download-specific yt-dlp args used for the download operation from the parsed config file and add them to the general yt-dlp args
+    mapfile -t download_specific_yt_dlp_args < <(cat "$VIDSIFT_DATA_DIR"/parsed_config.json | jq -r '.video_download."yt-dlp_args"[]')
+    cat "$VIDSIFT_DATA_DIR"/parsed_config.json | jq -r '.video_download."yt-dlp_args"[]'
+}
+
 function rename_dest_path {
     # if the user has the file renamer, rename it
     if [[ -x /usr/local/bin/rename_one_file.sh ]]; then
@@ -33,14 +44,12 @@ function rename_dest_path {
 }
 
 function main {
+    init "$@"
     # cd to target dir
     cd "$2"
     yt-dlp \
-        -f "bestvideo+bestaudio/best" \
-        --merge-output-format mkv \
-        --fragment-retries 10 \
-        --retries 10 \
-        --cookies-from-browser firefox \
+        "${yt_dlp_args[@]}" \
+        "${download_specific_yt_dlp_args[@]}" \
         --exec 'if [[ ! -x /usr/local/bin/rename_one_file.sh ]]; then
                     exit 0
                 fi
