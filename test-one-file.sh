e2ad4f2534c538cb6f3c8c1ee4999c502413f9db
daemonnd <daemonnd@protonmail.com>
2026-04-12 12:43:15 +0200

    add OUTPUT_MODE export to test-one-file.sh

--- test-one-file.sh
+++ test-one-file.sh
@@ -4,5 +4,6 @@ export VIDSIFT_DATA_DIR="$HOME/.local/share/vidsift"
 export VIDSIFT_CONFIG_DIR="$HOME/.config/vidsift"
 export VIDSIFT_HELPER_SCRIPTS_DIR="$HOME/.local/lib/vidsift/"
 export VIDSIFT_BIN_DIR="$HOME/.local/bin/"
+export OUTPUT_MODE=2
 
 ./parse_config.sh <"$VIDSIFT_CONFIG_DIR/config.jsonc" >"$VIDSIFT_DATA_DIR/parsed_config.json"
