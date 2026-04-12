4c95a758d3a5254b1d62299661ca158f54980ffb
daemonnd <daemonnd@protonmail.com>
2026-04-12 11:47:04 +0200

    add /bin to the vidsift bin dir for proper testing

--- test-one-file.sh
+++ test-one-file.sh
@@ -3,6 +3,6 @@
 export VIDSIFT_DATA_DIR="$HOME/.local/share/vidsift"
 export VIDSIFT_CONFIG_DIR="$HOME/.config/vidsift"
 export VIDSIFT_HELPER_SCRIPTS_DIR="$HOME/.local/lib/vidsift/"
-export VIDSIFT_BIN_DIR="$HOME/.local"
+export VIDSIFT_BIN_DIR="$HOME/.local/bin/"
 
 ./parse_config.sh <"$VIDSIFT_CONFIG_DIR/config.jsonc" >"$VIDSIFT_DATA_DIR/parsed_config.json"
